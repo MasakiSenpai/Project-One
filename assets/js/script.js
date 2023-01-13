2f3e034bbeed98eb26fee216bbcf73275d85e001
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-01-13 11:06:21 -0600

    added function to dynamically generate calendar days

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var quoteElement = document.getElementById("quote-box");
 var calendarContainerEl = document.getElementById("calendar-container");
 var dayNamesEl = document.getElementById("day-names");
 var dayRowContainerEl = document.getElementById("day-row-container");
+var daysContainer = document.getElementById("days");
 
 function getQuote() {
     fetch(quoteApi)
@@ -23,29 +24,47 @@ function getQuote() {
         })
 }
 
-function getRows() {
-    var dayRows = dayRowContainerEl.children
-    // gets all rows
-    console.log(dayRows);
-    // gets the children of the first row
-
-    // get each row
-    for (var i = 0; i < dayRows.length; i++) {
-        console.log('children of row ' + i)
-        // get each day in each row
-        getDays(i);
+function makeDays() {
+    for (var i = 0; i < 31; i++) {
+        var button = document.createElement('button')
+        button.setAttribute('id', 'day-button');
+        // button.setAttribute('class', 'col p-5');
+        // button.setAttribute('style', 'height: 100%')
+        // button.setAttribute('style', 'width: 14%')
+        button.textContent = i + 1;
+        dayRowContainerEl.appendChild(button);
     }
 }
 
-function getDays(i) {
-    var dayRows = dayRowContainerEl.children
-    var row = dayRows[i].children
-    console.log('summary' + row)
-    // console.log each child of each row
-    for (var d = 0; d < row.length; d++) {
-        console.log(row[d]);
-        row[d].textContent = d + 1
-    }
+function displayMonth() {
+
 }
+
+// function getRows() {
+//     var dayRows = dayRowContainerEl.children
+//     // gets all rows
+//     console.log(dayRows);
+//     // gets the children of the first row
+
+//     // get each row
+//     for (var i = 0; i < dayRows.length; i++) {
+//         console.log('children of row ' + i)
+//         // get each day in each row
+//         getDays(i);
+//     }
+// }
+
+// function getDays(i) {
+//     var dayRows = dayRowContainerEl.children
+//     var row = dayRows[i].children
+//     console.log('summary' + row)
+//     // console.log each child of each row
+//     for (var d = 0; d < row.length; d++) {
+//         console.log(row[d]);
+//         var num = 1
+//         row[d].textContent = num;
+//     }
+// }
 // getQuote();
-getRows();
+// getRows();
+makeDays();
