962e4cba7d2674a27ea9e583a79e8a56d24503e0
Nya Tyus <nyastyus@gmail.com>
2023-01-13 10:46:41 -0500

    got quote to appear on page using javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var quoteApi = "https://api.goprogram.ai/inspiration";
+var quoteElement = document.getElementById("quote-box");
+function getQuote() {
+    fetch(quoteApi)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            var author = data.author;
+            var quote = data.quote;
+            var authorEl = document.createElement("p");
+            var quoteEl = document.createElement("p");
+            authorEl.textContent = author;
+            quoteEl.textContent = quote;
+            quoteElement.appendChild(quoteEl);
+            quoteElement.appendChild(authorEl);
+        })
+}
+getQuote();
