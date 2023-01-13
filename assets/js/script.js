f18b8df9606d2745b18a2af6b958f8cc9c8ac516
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-01-13 10:34:11 -0600

    started functions to target the days of the week

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var quoteApi = "https://api.goprogram.ai/inspiration";
 var quoteElement = document.getElementById("quote-box");
+var calendarContainerEl = document.getElementById("calendar-container");
+var dayNamesEl = document.getElementById("day-names");
+var dayRowContainerEl = document.getElementById("day-row-container");
+
 function getQuote() {
     fetch(quoteApi)
         .then(function (response) {
@@ -11,11 +15,37 @@ function getQuote() {
             var authorEl = document.createElement("p");
             var quoteEl = document.createElement("p");
 
-            
+
             authorEl.textContent = author;
             quoteEl.textContent = quote;
             quoteElement.appendChild(quoteEl);
             quoteElement.appendChild(authorEl);
         })
 }
-getQuote();
+
+function getRows() {
+    var dayRows = dayRowContainerEl.children
+    // gets all rows
+    console.log(dayRows);
+    // gets the children of the first row
+
+    // get each row
+    for (var i = 0; i < dayRows.length; i++) {
+        console.log('children of row ' + i)
+        // get each day in each row
+        getDays(i);
+    }
+}
+
+function getDays(i) {
+    var dayRows = dayRowContainerEl.children
+    var row = dayRows[i].children
+    console.log('summary' + row)
+    // console.log each child of each row
+    for (var d = 0; d < row.length; d++) {
+        console.log(row[d]);
+        row[d].textContent = d + 1
+    }
+}
+// getQuote();
+getRows();
